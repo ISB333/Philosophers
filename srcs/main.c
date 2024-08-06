ce02741c2b6d82927beeddc70c905d110ff179c3
Alexandre Desille <adesille@k2r3p7.42mulhouse.fr>
2024-08-06 12:54:10 +0200

    2024-08-06 12:54

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isb3 <isb3@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adesille <adesille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 08:48:04 by adesille          #+#    #+#             */
-/*   Updated: 2024/07/18 08:46:15 by isb3             ###   ########.fr       */
+/*   Updated: 2024/08/06 12:30:17 by adesille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@
 			! -> Philo 1 sits next to nbr_of_philo
 		! - To prevent philosophers from duplicating forks, 
 			! you should protect the forks state with a mutex for each of them
+
 		! - They should never die starving
 		! - They don't speak to each other
 		! - They don't know if another is about to die
@@ -45,11 +46,6 @@ static	int is_philo_dead(int n)
 		y_or_no = 1;
 	return (y_or_no);
 }
-
-// int	death_protect()
-// {
-
-// }
 
 void	*unlocker(void **m)
 {
@@ -66,8 +62,10 @@ void	*sleeping_time(t_philo *ph)
 	if (is_philo_dead(0))
 		return (unlocker((void *[]){&ph->f.state_mutex, NULL}));
 	pthread_mutex_unlock(&ph->f.state_mutex);
+
 	printf("Philosopher %d is sleeping.\n", ph->id);
 	usleep(ph->i.sleeping_time);
+
 	pthread_mutex_lock(&ph->f.state_mutex);
 	if (is_philo_dead(0))
 		return (unlocker((void *[]){&ph->f.state_mutex, NULL}));
@@ -85,13 +83,14 @@ void	*eating_time(t_philo *ph, struct timeval *current_time)
 	right = (ph->id + 1) % ph->f.nbr_of_philo;
 	pthread_mutex_lock(&ph->f.forks[left]);
 	pthread_mutex_lock(&ph->f.forks[right]);
-	gettimeofday(current_time, NULL);
-	ph->dying_time = (*current_time).tv_usec + ph->i.true_dying_time;
+	
 	// printf(BLUE"philo %d dying time = %ld\n"DEF, ph->id, ph->dying_time);
 	pthread_mutex_lock(&ph->f.state_mutex);
 	if (is_philo_dead(0))
 		return (unlocker((void *[]){&ph->f.state_mutex, &ph->f.forks[left],
 				&ph->f.forks[right], NULL}));
+	gettimeofday(current_time, NULL);
+	ph->dying_time = (*current_time).tv_usec + ph->i.true_dying_time;
 	pthread_mutex_unlock(&ph->f.state_mutex);
 
 	// printf(GREEN"time of the day %ld\n"DEF, current_time.tv_usec);
@@ -111,29 +110,42 @@ void	*philo_diner_table(void *num)
 	ph->dying_time = current_time.tv_usec + ph->i.true_dying_time;
 	while (1)
 	{
-		gettimeofday(&current_time, NULL);
-		if (current_time.tv_usec >= ph->dying_time)
-		{
-			pthread_mutex_lock(&ph->f.state_mutex);
-			is_philo_dead(1);
-			pthread_cond_broadcast(&ph->f.state_cond);
-			printf(RED"Philosopher %d is DEAD.\n"DEF, ph->id);
-			return (unlocker((void *[]){&ph->f.state_mutex, NULL}));
-		}
-
 		pthread_mutex_lock(&ph->f.state_mutex);
         if (is_philo_dead(0))
 			return (unlocker((void *[]){&ph->f.state_mutex, NULL}));
         pthread_mutex_unlock(&ph->f.state_mutex);
-
-		if (!eating_time(ph, &current_time))
+	
+		if (!is_philo_dead(0) && !eating_time(ph, &current_time))
 			return (NULL);
 	
-		if (!sleeping_time(ph))
+		if (!is_philo_dead(0) && !sleeping_time(ph))
 			return (NULL);
+
+		pthread_mutex_lock(&ph->f.state_mutex);
+		gettimeofday(&current_time, NULL);
+		long current_time_ms = (current_time.tv_sec * 1000) + (current_time.tv_usec / 1000);
+		if (current_time_ms >= ph->dying_time && !is_philo_dead(0))
+		{
+			// pthread_mutex_lock(&ph->f.state_mutex);
+			pthread_cond_broadcast(&ph->f.state_cond);
+			is_philo_dead(1);
+			unlocker((void *[]){&ph->f.state_mutex, NULL});
+			printf(RED"Philosopher %d is DEAD.\n"DEF, ph->id);
+			return (NULL);
+			// return (unlocker((void *[]){&ph->f.state_mutex, NULL}));
+		}
+        pthread_mutex_unlock(&ph->f.state_mutex);
 	}
 	return (NULL);
 }
+
+// void monitor_philosophers(t_forks *f) {
+//     pthread_mutex_lock(&f->state_mutex);
+//     while (!is_philo_dead(0)) {
+//         pthread_cond_wait(&f->state_cond, &f->state_mutex);
+//     }
+//     pthread_mutex_unlock(&f->state_mutex);
+// }
 
 int	thread_maker(t_init i)
 {
@@ -155,6 +167,7 @@ int	thread_maker(t_init i)
 		if (init_philo(&p, k, f, i))
 			return (1);
 	}
+	// monitor_philosophers(&f);
 	joiner(p);
 
     pthread_mutex_destroy(&f.state_mutex);
@@ -174,5 +187,6 @@ int	main(int argc, char *argv[])
 	}
 	else
 		return (error("wrong number of arguments\n"), 1);
-	return (0);
+	printf("-----------------------------\n\n");
+	return (mem_manager(0, 0, 'C'), 0);
 }
